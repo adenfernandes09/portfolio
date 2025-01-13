5cecb9b2db8c36f9268630252b833de306ae61a4
Aden Fernandes <adenferns999@gmail.com>
2025-01-13 17:48:07 +0530

    feat: Added contact page
    - Added the form which houses the contact information
    - Added the styling for said form

--- app/contact/page.jsx
+++ app/contact/page.jsx
@@ -1,7 +1,78 @@
-const page = () => {
-  return (
-    <div>page</div>
-  )
-}
+"use client";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
 
-export default page
+import {
+  Select,
+  SelectTrigger,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectLabel,
+  SelectValue,
+} from "@/components/ui/select";
+
+import { FaPhoneAlt, FaEnvelope, FaMapMarkerAlt } from "react-icons/fa";
+import { motion } from "framer-motion";
+
+const info = [
+  { title: "Phone", value: "+353892200290", icon: <FaPhoneAlt /> },
+  { title: "Email", value: "fernandes.aden09@gmail.com", icon: <FaEnvelope /> },
+  { title: "Address", value: "Ringsend, Irishtown, Dublin", icon: <FaMapMarkerAlt /> },
+];
+
+const Contact = () => {
+  return (
+    <motion.section
+      initial={{ opacity: 0 }}
+      animate={{
+        opacity: 1,
+        transition: { delay: 2.4, duration: 0.4, ease: "easeInOut" },
+      }}
+    >
+      <div className="container mx-auto">
+        <div className="flex flex-col xl:flex-row gap-[30px]">
+          <div className="xl:h-[54%] order-2 xl:order-none">
+            <form className="flex flex-col bg-[#27272c] p-10 rounded-xl">
+              <h3 className="text-4xl text-accent-default">Get in Touch</h3>
+              <p className="text-white/60">
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Hic
+                nemo modi eius fuga, quam eveniet autem exercitationem a quidem
+                error at cum amet labore, explicabo magni, aut nulla alias
+                nostrum.
+              </p>
+              <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
+                <Input type="firstname" placeholder="First Name" />
+                <Input type="lastname" placeholder="Last Name" />
+                <Input type="Email" placeholder="Email" />
+                <Input type="Phone" placeholder="Phone" />
+                <Button size="md" className="max-w-40">
+                  Submit
+                </Button>
+              </div>
+            </form>
+          </div>
+          <div className="flex flex-1 items-center xl:justify-end order-1 xl:order-none mb-8 xl:mb-0">
+            <ul className="flex flex-col gap-10">
+              {info.map((item, index) => {
+                return (
+                  <li key={index} className="flex items-center gap-6">
+                    <div className="w-[52px] h-[52px] xl:w-[72px] xl:h-[72px] bg-[#27272c] rounded-md text-accent-default flex items-center justify-center">
+                      <div className="text-[28px]">{item.icon}</div>
+                    </div>
+                    <div className="flex-1">
+                      <p className="text-white/60">{item.title}</p>
+                      <h3 className="text-xl">{item.value}</h3>
+                    </div>
+                  </li>
+                );
+              })}
+            </ul>
+          </div>
+        </div>
+      </div>
+    </motion.section>
+  );
+};
+
+export default Contact;
